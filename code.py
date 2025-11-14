bead55528f973993012db5b641e1d05bd7ced499
hoodasandeep150-wq <hoodasandeep150@gmail.com>
2025-11-14 05:51:53 -0800

    Add files via upload

---
+++ code.py
@@ -0,0 +1,11 @@
+a = float(input('enter the first number'))
+b=float(input('enter the second number'))
+addition= a+b
+subtraction = a-b
+multiplication=a*b
+division= a/b
+print("Addition :",addition)
+print("subtraction :",subtraction)
+print("multiplication :",multiplication)
+ 
+
